f72be91a9636cfa003f8e05cade61433311ac804
Heinrich <113572382+henryleo180@users.noreply.github.com>
2025-04-29 14:25:49 -0400

    move to other branch, stash current change

--- packages/application/cvrp/build.sh
+++ packages/application/cvrp/build.sh
@@ -12,4 +12,8 @@ make -j$(nproc)
 
 cd ..
 
-./bin/cvrp instance/P-n20-k2.vrp -u 217
+# ./bin/cvrp instance/P-n20-k2.vrp -u 217
+
+./bin/cvrp /home/haoran/data_eda/instance2/180_200/CVRP_180_115.vrp -u 28802 2>&1 | tee tmp.txt
+
+# ./bin/cvrp instance/P-n60-k15.vrp -u 979
